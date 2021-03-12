a34b7412783df88b624ffee18a37af8175a25e94
royweb <xytank@gmail.com>
2021-03-12 14:39:15 -0500

    Add login page with styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="app">
-      <h1>WRI Music List</h1>
+      <Login />
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './Login.css';
+
+function Login() {
+    return (
+        <div className="login">
+            <img
+                src="/logo.png"
+                alt="Logo"
+            />
+            <a>LOGIN WITH SPOTIFY</a>
+        </div>
+    );
+}
+
+export default Login;
