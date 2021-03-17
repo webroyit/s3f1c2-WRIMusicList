de3564e77c690c4b1ae19ed0dc2924ec21710115
royweb <xytank@gmail.com>
2021-03-17 15:56:55 -0400

    Display user name and image from spotify

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,11 @@ import { Avatar } from "@material-ui/core";
 import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
 
 import './Header.css';
+import { useDataLayerValue} from '../DataLayer';
 
 function Header() {
+    const [{ user }, dispatch] = useDataLayerValue();
+
     return (
         <div className="header">
             <div className="header__left">
@@ -16,8 +19,8 @@ function Header() {
             </div>
 
             <div className="header__right">
-                <Avatar src="1" alt="RW"  />
-                <h4>Roy Web</h4>
+                <Avatar src={user?.images[0]?.url} alt={user?.display_name} />
+                <h4>{user?.display_name}</h4>
             </div>
         </div>
     );
