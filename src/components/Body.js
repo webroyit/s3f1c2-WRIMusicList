5f570342f420ee06a31c1ad5b2d6eaad0891ba74
royweb <xytank@gmail.com>
2021-03-18 14:45:50 -0400

    Add songs list with styles

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 import './Body.css';
 import { useDataLayerValue} from '../DataLayer';
 import Header from './Header';
+import SongRow from './SongRow';
 
 function Body({ spotify }) {
     const [{ discover_weekly }, dispatch] = useDataLayerValue();
-    
+
     return (
         <div className="body">
             <Header spotify={spotify} />
@@ -18,6 +22,19 @@ function Body({ spotify }) {
                     <h2>Discover Weekly</h2>
                     <p>{discover_weekly?.description}</p>
                 </div>
+            </div>
+
+            <div className="body__songs">
+                <div className="body__icons">
+                    <PlayCircleFilledIcon className="body__shuffle" />
+                    <FavoriteIcon fontSize="large" />
+                    <MoreHorizIcon />
+                </div>
+
+                {/* List of songs */}
+                {discover_weekly?.tracks.items.map(item => (
+                    <SongRow track={item.track} />
+                ))}
             </div>
         </div>
     );
---
+++ src/components/SongRow.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './SongRow.css';
+
+function SongRow({ track }) {
+    return (
+        <div className="songRow">
+            <img className="songRow__album" src={track.album.images[0].url} alt="Song Image" />
+            <div className="songRow__info">
+                <h2>{track.name}</h2>
+                <p>
+                    {track.artists.map((artist) => artist.name).join(", ")} -{" "}
+                    {track.album.name}
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default SongRow;
